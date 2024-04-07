0641e15b35c4d377c927cc116835582e283e101e
devmusiczz <onlyfordance0@gmail.com>
2024-04-07 12:19:58 +0530

    creator buddy

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -43,7 +43,7 @@ export async function GET() {
           price_data: {
             currency: "INR",
             product_data: {
-              name: "AIBundle Pro",
+              name: "CreatorBuddy Pro",
               description: "Unlimited AI Generations"
             },
             unit_amount: 19900,
